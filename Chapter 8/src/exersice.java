91173c666dd7150a344601f96444f083f88225cc
HannahSwigart6 <hannahdeanswigart@gmail.com>
2017-10-03 09:28:11 -0500

    update

---
+++ Chapter 8/src/exersice.java
@@ -0,0 +1,36 @@
+import java.util.Collections;
+
+public class exersice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num []= {23,6,47,35,2,14};
+		int average, highest;
+		int count = 0;
+		int score = 0
+		while( num != quit)
+		{
+			num[count] = score;
+			
+			total += scores[count];
+			++count;
+			
+			if(count == max)
+				score = quit;
+			else
+				System.out.println("Enter next quix score or " +quit+ " to quit >>");
+			score = input.nextInt();
+			
+		}		
+		System.out.print("The score entered were: ");
+		for (int x = 0; x <count; ++x)
+			System.out.println(scores[x] + " ");
+		if(count != 0)
+			System.out.println("\n The Average is " + (total * 1)/count);
+		else 
+			System.out.println("No scores entered ");
+			
+		
+	
+}
+}
